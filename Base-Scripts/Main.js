a5d588d54719003905d8928f78c1d8c3aae38ba2
desi <desi@desi-Lenovo-IdeaPad-S300>
2013-06-16 00:50:31 -0500

    Register dialog

--- Base-Scripts/Main.js
+++ Base-Scripts/Main.js
@@ -1,11 +1,10 @@
 var home = {
     homeSetup : function (){
-        var form = "<form id='register-form' class='form-signin'>"+
-        +"<h2 class='form-signin-heading'>Please sign in</h2>"+
-        +"<input type='text' class='input-block-level' placeholder='Correo electronico'>"+
-        +"<input type='password' class='input-block-level' placeholder='Password'>"+
-        +"<button class='btn btn-large btn-primary text-right' type='submit'>Sign in</button>"+
-        +"</form>";
+        var form = "<form id='register-form' class='form-signin'>\n\
+        <h2 class='form-signin-heading'>Please sign in</h2><input type='text' class='input-block-level' placeholder='Correo electronico'>\n\
+        <input type='password' class='input-block-level' placeholder='Password'>\n\
+        <button class='btn btn-large btn-primary text-right' type='submit'>Sign in</button>\n\
+        </form>";
         $("body").append(form);
         $("#register-form").dialog({modal:true});
     }
