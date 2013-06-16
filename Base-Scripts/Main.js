072e33e487195a09ead0ac513b8ed2865aa56585
desi <desi@desi-Lenovo-IdeaPad-S300>
2013-06-16 01:04:56 -0500

    Send register ajax

--- Base-Scripts/Main.js
+++ Base-Scripts/Main.js
@@ -1,12 +1,36 @@
 var home = {
     homeSetup : function (){
-        var form = "<form id='register-form' class='form-signin'>\n\
-        <h2 class='form-signin-heading'>Please sign in</h2><input type='text' class='input-block-level' placeholder='Correo electronico'>\n\
-        <input type='password' class='input-block-level' placeholder='Password'>\n\
-        <button class='btn btn-large btn-primary text-right' type='submit'>Sign in</button>\n\
+        var form = "<form id='register-form' title='Registro' class='form-signin'>\n\
+        <h2 class='form-signin-heading'>Registra tu empresa</h2>\n\
+        <input id='reg-company-name' type='text' class='input-block-level' placeholder='Nombre de la empresa'>\n\
+        <input id='reg-email' type='text' class='input-block-level' placeholder='Correo electronico'>\n\
+        <input id='reg-pass' type='password' class='input-block-level' placeholder='Contraseña'>\n\
+        <button id="reg-btn" class='btn btn-large btn-primary text-right' type='submit'>Registrar</button>\n\
         </form>";
         $("body").append(form);
         $("#register-form").dialog({modal:true});
+        $("#reg-btn").click(home.register);
+    },
+            
+    register : function(){
+        var args = {
+                action:"put",
+                name:$("#reg-email").val(),
+                lastName:$("#reg-company-name").val(),
+                password:$("#reg-pass").val()
+        };
+        
+        $.ajax({ 
+            url:"test-aplications/profile/user",
+            dataType:"json",type:"POST", 
+            data: args, 
+            success:function(data){
+                console.log(JSON.stringify(data));
+            }, 
+            error:function(data){
+                console.log(JSON.stringify(data));
+            }
+        });
     }
 };
 
