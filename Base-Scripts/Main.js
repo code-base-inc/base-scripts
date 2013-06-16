fd89812d1d5b0a19ac3b5745cfb3a28d0a8ca57f
desi <desi@desi-Lenovo-IdeaPad-S300>
2013-06-16 01:09:48 -0500

    Send register ajax

--- Base-Scripts/Main.js
+++ Base-Scripts/Main.js
@@ -1,12 +1,12 @@
 var home = {
     homeSetup : function (){
-        var form = "<form id='register-form' title='Registro' class='form-signin'>\n\
+        var form = "<div id='register-form' title='Registro' class='form-signin'>\n\
         <h2 class='form-signin-heading'>Registra tu empresa</h2>\n\
         <input id='reg-company-name' type='text' class='input-block-level' placeholder='Nombre de la empresa'>\n\
         <input id='reg-email' type='text' class='input-block-level' placeholder='Correo electronico'>\n\
         <input id='reg-pass' type='password' class='input-block-level' placeholder='Contraseña'>\n\
         <button id='reg-btn' class='btn btn-large btn-primary text-right' type='submit'>Registrar</button>\n\
-        </form>";
+        </div>";
         $("body").append(form);
         $("#register-form").dialog({modal:true});
         $("#reg-btn").click(home.register);
