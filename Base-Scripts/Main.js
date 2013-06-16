265da0e38e0c14723b3e5a4bbf60a4ea1e69b785
desi <desi@desi-Lenovo-IdeaPad-S300>
2013-06-16 00:39:01 -0500

    Register dialog

--- Base-Scripts/Main.js
+++ Base-Scripts/Main.js
@@ -1,6 +1,16 @@
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-console.log("loading script...");
-// Testing comit
+var home = {
+    homeSetup : function (){
+        var form = "<form id='register-form' class='form-signin'>"+
+        +"<h2 class='form-signin-heading'>Please sign in</h2>"+
+        +"<input type='text' class='input-block-level' placeholder='Correo electronico'>"+
+        +"<input type='password' class='input-block-level' placeholder='Password'>"+
+        +"<button class='btn btn-large btn-primary text-right' type='submit'>Sign in</button>"+
+        +"</form>";
+        $("body").append(form);
+        $("#register-form").dialog({modal:true});
+    }
+};
+
+$(function(){
+    $(".link-register").click(home.homeSetup);
+});
